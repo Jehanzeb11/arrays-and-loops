275ccf991cd2902762f86ce9a2b1e214b6555335
jehanzebsiddiqui287@gmail.com <jehanzebsiddiqui287@gmail.com>
2022-04-06 02:18:49 +0500

    arrays and loops done

---
+++ js arrays and loops/arrays.js
@@ -0,0 +1,356 @@
+// question 1
+
+// let arr = new Array();
+
+
+
+
+// question 2
+
+
+
+
+
+
+// question 3
+
+// let arr = ["name1","name2","name3"]
+
+
+
+
+
+// question 4
+
+// let arr = [1,2,3]
+
+
+
+
+
+
+
+//question 5
+
+// let arr = [true,false]
+
+
+
+
+
+
+
+
+// question 6
+
+// let arr = ["Jehanzeb",123,true,null,undefined]
+
+
+
+
+
+
+
+// question 7
+
+// let arr = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil", "PhD"]
+
+
+// document.write("<h1>Qualifications:</h1>");
+
+// for(let i =0;i<arr.length;i++){
+//     document.write(arr[i]+"<br>")
+// }
+
+
+
+
+
+
+// question 8
+
+// let arr = ["michael","john","tony"];
+
+// let scr = [320,230,480]
+
+// for(i=0;i<arr.length;i++){
+//     for(j = 0;j<scr.length;j++){
+//         document.write("score of "+arr[i]+"is "+scr[j]+" percentage: "+scr[j]/500*100+"<br>")
+        
+//     }
+// break;
+// }
+
+
+
+
+
+
+
+// question 9
+
+// let clr = ["red","green","blue"];
+
+// document.write(clr+"<br>"+"<br>"+"<br>"+"<br>");
+
+
+// a 
+
+// let ask = prompt("enter color to add in the beginning");
+
+// clr.unshift(ask)
+
+// document.write(clr+"<br> <br> <br>");
+
+
+// b
+
+
+// let inp = prompt("enter color to add in the end");
+
+// clr.push(inp)
+
+// document.write(clr);
+
+
+// c
+
+
+
+// let firstclr = prompt("enter 1st color to add in the beginning");
+
+// let twoclr = prompt("enter 2nd color to add in the beginning");
+
+// clr.unshift(firstclr)
+// clr.unshift(twoclr)
+
+// document.write(clr);
+
+
+
+// d
+
+// clr.shift()
+// document.write(clr)
+
+
+
+
+
+
+// e
+
+// clr.pop()
+// document.write(clr)
+
+
+
+
+
+
+
+// f
+
+// let indexClr = prompt("enter index of color to add");
+
+// let color = prompt("enter color")
+
+// clr.splice(indexClr,0,color)
+
+
+// document.write(clr);
+
+
+
+
+
+
+
+// g
+
+// let firstclr = prompt("enter index color to remove ");
+
+// let twoclr = prompt("enter how many color you want to remove");
+
+// clr.splice(firstclr,twoclr)
+
+// document.write(clr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 10
+
+
+// let arr = [320,230,480,120]
+
+
+// document.write("score of students "+arr)
+
+// let srt = arr.sort()
+
+// document.write("ordered score of students "+srt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 11
+
+// let cities = ["karachi","peshawar","islamabad","lahore","quetta"]
+
+
+// let copy = cities.slice(1,3)
+
+
+// document.write(cities+"<br>")
+
+
+// document.write(copy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 12
+
+
+// var arr = ["This" , "is" , "my" , "cat"];
+
+
+// document.write("<h1>Array:</h1>")
+// document.write(arr+"<br>"+"<br>"+"<br>")
+
+// var arrJoin = arr.join(" ")
+
+
+
+
+// document.write("<h1>String:</h1>")
+
+
+// document.write(arrJoin)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 13
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 14
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 15
+
+// let arr = ["Apple","Samsung","Motorola","Nokia","Sony","Haier"];
+
+// for(i=0;i<arr.length;i++){
+//     document.write("<select>"+"<option>"+arr[i]+"</option>"+"</select>"+"<br>")
+// }
---
+++ js arrays and loops/loops with arrays.js
@@ -0,0 +1,269 @@
+// question 1
+
+//  var arr = [[],[],[],[]];
+
+
+
+
+
+
+
+
+// question 2
+
+// var arr = [
+// [0,1,2,1],
+// [1,0,1,2],
+// [2,1,0,1]
+// ];
+
+
+// for(let i = 0;i<arr.length;i++){
+//     for(j=0;j<arr.length;j++){
+//         document.write(arr[j][i])
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+// question 3
+
+// for(let i = 0;i<=10;i++){
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// question 4
+
+
+// let pro = +prompt("enter number to print table")
+
+// let len = +prompt("enter length of number")
+
+
+// for(i =0;i<=len;i++){
+//     document.write(pro+" x "+i+" = "+pro*i+"<br>")
+// }
+
+
+
+
+
+
+
+
+
+
+
+// question 5
+
+
+// fruits = ["apple", "banana", "mango", "orange", "strawberry"]
+
+
+
+// for(let i = 0;i<fruits.length;i++){
+//     document.write("Element at index: "+i+" "+fruits[i]+"<br>")
+// }
+
+
+
+
+
+
+
+
+    // question 6
+
+
+// for(let i = 0;i<=10;i++){
+//     console.log(i)
+// }
+
+
+
+
+
+// for(let i = 0;i<=10;i--){
+//     console.log(i)
+// }
+
+
+
+
+// for(let i = 0;i<=20;i +=2){
+//     console.log(i)
+// }
+
+
+
+
+
+
+
+// for(let i = 1;i<=20;i += 2){
+//     console.log(i)
+// }
+
+
+
+// for(let i = 0;i<=20;i +=2){
+//     console.log(i+"k")
+// }
+
+
+
+
+
+
+
+
+
+
+
+// question 7
+
+// let A = ["cake", "apple pie", "cookie", "chips", "patties"]
+
+
+// let search = prompt("enter your item")
+
+
+// for(i=0;i<A.length;i++){
+//     if(search ==A[i]){
+//         alert(A[i]+" is available in our bakery")
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 8
+
+// let A = [24, 53, 78, 91, 12];
+
+// let max = -Infinity;
+
+// for(let i =0;i<A.length;i++){
+//     if(A[i]>max){
+//       max = A[i]
+//     }
+// }
+
+// console.log(max)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 9
+
+
+
+// let A = [24, 53, 78, 91, 12];
+
+// let small = Infinity;
+
+// for(let i =0;i<A.length;i++){
+//     if(A[i]<small){
+//       small = A[i]
+//     }
+// }
+
+// console.log(small)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// question 10
+
+
+
+// for(let i = 5;i<=100;i +=5){
+//         document.write(i+"<br>")
+//     }
